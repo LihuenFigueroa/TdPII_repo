e45b50c4bb3da085714fe17ab261e1c0ec68e53c
LihuenFigueroa <lihuenfigueroa@gmail.com>
2019-09-10 01:01:59 -0300

    a ver si escucha jenkins

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld{
     public static void main (String[]args){
-        System.out.println("JENKINS ME ESTA ESCUCHANDO!");
+        System.out.println("JENKINS ME ESTA ESCUCHANDO!!!");
     }
 }
