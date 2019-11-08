166f4caf4e9e3c3a08360f85c02bd9f006a3402c
LihuenFigueroa <lihuenfigueroa@gmail.com>
2019-11-07 21:20:22 -0300

    Testing .c

--- c_to_hex.sh
+++ c_to_hex.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
+avr-gcc -g -Os -mmcu=atmega328p -c led.c
+avr-gcc -g -mmcu=atmega328p -o led.elf led.o
+avr-objcopy -j .text -j .data -O ihex led.elf led.hex
 avr-objcopy -O ihex -R .eeprom led.elf led.hex
 
 /home/lihuen/Downloads/arduino-1.8.10/hardware/tools/avr/bin/avrdude -C/home/lihuen/Downloads/arduino-1.8.10/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:/tmp/arduino_build_260289/my_blink.ino.hex:i 
