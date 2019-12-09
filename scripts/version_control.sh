bd7c80617fcb42b547dc28392664ced16366a6a8
LihuenFigueroa <lihuenfigueroa@gmail.com>
2019-12-09 11:05:27 -0300

    esp8266

--- scripts/version_control.sh
+++ scripts/version_control.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-program_name=git log -1 | sed '5!d' | cut -d " " -f 5,6,7,8,9,10,11,12,13,14,15
+program_name=$(git log -1 | sed '5!d' | cut -d " " -f 5,6,7,8,9,10,11,12,13,14,15) 
 
 cd programs
 
-is_created= find . -type d -name $program_name
+is_created=$(find . -type d -name $program_name)
 
-if [[ -z $is_created ]]; then
-    mkdir "$program_name"
+if [ -z $is_created ]; then
+    mkdir $program_name
+    echo Creando carpeta...
+fi 
 
 cd $program_name
 
