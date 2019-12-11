5aa0e1a68c07e5ed4e692125705ed490106f004b
LihuenFigueroa <lihuenfigueroa@gmail.com>
2019-12-11 18:13:24 -0300

    Blinky#delay100

--- Blinky/Blinky.ino
+++ Blinky/Blinky.ino
@@ -1,5 +1,5 @@
 
-int delay_time=1000;
+int delay_time=100;
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
